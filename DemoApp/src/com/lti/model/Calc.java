c9dfaf591b768f079125bc6391230ab169ab3a94
Ruthvik Roy <ruthvikroy.chinna@gmail.com>
2020-12-18 16:49:55 +0530

    changed muyltiply

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -9,7 +9,7 @@ public class Calc {
 		System.out.println("Difference = " + (num1 - num2));
 	}
 	public void multiply(int num1, int num2) {
-		System.out.println("Product = " + (num1 * num2));
+		System.out.println("Product is  = " + (num1 * num2));
 	}
 	
 }
