1ac7b069cd57f071e6fb3190f7fd71d8a984568e
Ruthvik-Roy <76148914+Ruthvik-Roy@users.noreply.github.com>
2020-12-18 16:58:59 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -12,5 +12,9 @@ public class Calc {
 public void division(int num1, int num2) {
 		System.out.println("Difference = " + (num1 / num2));
 	}	
+	
+public void multiply(int num1, int num2) {
+	System.out.println("<ultiplication = " + (num1 * num2));
+}	
  
 }
