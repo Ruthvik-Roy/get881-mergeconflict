6569a9eec0c43da7c0bf1debcd2ab0c59ce5777b
Ruthvik Roy <ruthvikroy.chinna@gmail.com>
2020-12-18 16:07:01 +0530

    new commit

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,11 @@
+package com.lti.model;
+
+public class Sample {
+	public void show() {
+		System.out.println("Hello World");
+	}
+	
+	public void display() {
+		System.out.println("Hello Friends");
+	}
+}
