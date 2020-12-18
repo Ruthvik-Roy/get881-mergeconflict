78b7b3de852f316f2231c28ec311f76c409dde1d
Ruthvik-Roy <76148914+Ruthvik-Roy@users.noreply.github.com>
2020-12-18 16:47:58 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,11 +4,13 @@ public class Calc {
 	public void add(int num1, int num2) {
 
 		System.out.println("Summation = " + (num1 + num2));
-
 	}
 
 	public void sub(int num1, int num2) {
 		System.out.println("Difference = " + (num1 - num2));
 	}
+public void division(int num1, int num2) {
+		System.out.println("Difference = " + (num1 / num2));
+	}	
  
 }
