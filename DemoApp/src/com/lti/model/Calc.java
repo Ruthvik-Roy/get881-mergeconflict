23c8221eb8f0c05ad2fc28db4887bccf72b18505
Ruthvik Roy <ruthvikroy.chinna@gmail.com>
2020-12-18 16:45:34 +0530

    multiply method added

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,5 +8,8 @@ public class Calc {
 	public void sub(int num1, int num2) {
 		System.out.println("Difference = " + (num1 - num2));
 	}
- 
+	public void multiply(int num1, int num2) {
+		System.out.println("Difference = " + (num1 * num2));
+	}
+	
 }
